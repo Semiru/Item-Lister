72771e966662cd955eff65885e87446bc705bf7a
Cemil Kocaman <cemilkocaman@gmail.com>
2020-01-18 19:26:25 +0300

    Add initial project

---
+++ Item Lister/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Item Lister
+//
+//  Created by Cemil Kocaman on 18.01.2020.
+//  Copyright © 2020 Cemil Kocaman Software. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ Item ListerTests/Item_ListerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Item_ListerTests.swift
+//  Item ListerTests
+//
+//  Created by Cemil Kocaman on 18.01.2020.
+//  Copyright © 2020 Cemil Kocaman Software. All rights reserved.
+//
+
+@testable import Item_Lister
+import XCTest
+
+class Item_ListerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
