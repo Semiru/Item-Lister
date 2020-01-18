bf7623cc28be1170683d734745dbdbcc8b3ba442
Cemil Kocaman <cemilkocaman@gmail.com>
2020-01-18 21:00:19 +0300

    Add `UIColor+Styling` extension

---
+++ Item Lister/Common/Extensions/UIColor/UIColor+Styling.swift
@@ -0,0 +1,34 @@
+//
+//  UIColor+Styling.swift
+//  Item Lister
+//
+//  Created by Cemil Kocaman on 18.01.2020.
+//  Copyright © 2020 Cemil Kocaman Software. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Color Style Guide
+
+extension UIColor {
+
+    static var redColor: UIColor {
+        return color(withRed: 252.0, green: 26.0, blue: 40.0)
+    }
+
+    static var grayColor: UIColor {
+        return color(withRed: 171.0, green: 176.0, blue: 182.0)
+    }
+
+    static var whiteColor: UIColor {
+        return color(withRed: 255.0, green: 255.0, blue: 255.0)
+    }
+
+    static var blackColor: UIColor {
+        return color(withRed: 0.0, green: 0.0, blue: 0.0)
+    }
+
+    private static func color(withRed red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: 1.0)
+    }
+}
