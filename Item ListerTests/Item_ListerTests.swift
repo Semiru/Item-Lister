91ac240da249ecebaa96b53d510669624c2e279e
Cemil Kocaman <cemilkocaman@gmail.com>
2020-01-19 16:43:53 +0300

    Add Unit Tests

--- Item ListerTests/Item_ListerTests.swift
+++ Item ListerTests/Item_ListerTests.swift
@@ -9,26 +9,128 @@
 @testable import Item_Lister
 import XCTest
 
+let validURL = URL(string: "http://5e209e06e31c6e0014c60962.mockapi.io/api/products")
+let invalidURL = URL(string: "http://5e209e06e31c6e0014c60962.mockapi.io/api")
+
+var firstItem: Item!
+var lastItem: Item!
+var urlSession: URLSession!
+
 class Item_ListerTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+
+        firstItem = Item(id: "1",
+                    category: "Jewelery",
+                    name: "Unbranded Metal Pants",
+                    price: "591.00",
+                    image: "https://s3.amazonaws.com/uifaces/faces/twitter/mufaddal_mw/128.jpg")
+
+        lastItem = Item(id: "50",
+                        category: "Grocery",
+                        name: "Fantastic Metal Hat",
+                        price: "419.00",
+                        image: "https://s3.amazonaws.com/uifaces/faces/twitter/osmond/128.jpg")
+
+        urlSession = URLSession(configuration: .default)
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        firstItem = nil
+        lastItem = nil
+        urlSession = nil
+
+        super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func testValidCallToAPIForFetchingItems() {
+        let promise = expectation(description: "Fetch Items Succes For Valid URL")
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        var statusCode: Int?
+        var responseError: Error?
+
+        let dataTask = urlSession.dataTask(with: validURL!) { _, response, error in
+            statusCode = (response as? HTTPURLResponse)?.statusCode
+            responseError = error
+            promise.fulfill()
         }
+
+        dataTask.resume()
+        wait(for: [promise], timeout: 5)
+
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
     }
 
+    func testInvalidCallToAPIForFetchingItems() {
+        let promise = expectation(description: "Fetch Items Failure For Invalid URL")
+
+        var statusCode: Int?
+        var responseError: Error?
+
+        let dataTask = urlSession.dataTask(with: invalidURL!) { _, response, error in
+            statusCode = (response as? HTTPURLResponse)?.statusCode
+            responseError = error
+            promise.fulfill()
+        }
+
+        dataTask.resume()
+        wait(for: [promise], timeout: 5)
+
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 404)
+    }
+
+    func testFirstItemValidity() {
+        let promise = expectation(description: "Item Fetching Complete")
+
+        var firstData: Item?
+
+        let dataTask = urlSession.dataTask(with: validURL!) { data, _, _ in
+            guard let data = data,
+                let itemList = try? JSONDecoder().decode([Item].self, from: data) else {
+                    promise.fulfill()
+                    return
+            }
+
+            firstData = itemList[0]
+            promise.fulfill()
+        }
+
+        dataTask.resume()
+        wait(for: [promise], timeout: 5)
+
+        XCTAssertEqual(firstItem.id, firstData!.id)
+        XCTAssertEqual(firstItem.category, firstData!.category)
+        XCTAssertEqual(firstItem.name, firstData!.name)
+        XCTAssertEqual(firstItem.price, firstData!.price)
+        XCTAssertEqual(firstItem.image, firstData!.image)
+    }
+
+    func testLastItemValidity() {
+        let promise = expectation(description: "Item Fetching Complete")
+
+        var lastData: Item?
+
+        let dataTask = urlSession.dataTask(with: validURL!) { data, _, _ in
+            guard let data = data,
+                let itemList = try? JSONDecoder().decode([Item].self, from: data) else {
+                    promise.fulfill()
+                    return
+            }
+
+            lastData = itemList[itemList.count - 1]
+            promise.fulfill()
+        }
+
+        dataTask.resume()
+        wait(for: [promise], timeout: 5)
+
+        XCTAssertEqual(lastItem.id, lastData!.id)
+        XCTAssertEqual(lastItem.category, lastData!.category)
+        XCTAssertEqual(lastItem.name, lastData!.name)
+        XCTAssertEqual(lastItem.price, lastData!.price)
+        XCTAssertEqual(lastItem.image, lastData!.image)
+    }
 }
